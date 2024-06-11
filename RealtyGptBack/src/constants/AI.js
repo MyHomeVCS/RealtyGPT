74eafcf65070a59a5250ec61a8495c39cb4d6e31
Nahapet-sb <nahapet.papikyan@softconstruct.com>
2024-06-11 23:16:29 +0400

    init new back-end

---
+++ RealtyGptBack/src/constants/AI.js
@@ -0,0 +1,27 @@
+const BASIC_PARAMETERS = [
+  'floor',
+  'roomsCount',
+  'price',
+  'salary',
+  'apartmentArea',
+  'developer',
+]
+
+const BASIC_PARAMETERS_EXPLANATION = {
+  'floor': 'the level of the building where the apartment is located',
+  'roomsCount': 'the number of rooms in the apartment',
+  'price': 'the cost of the apartment',
+  'salary': 'the monthly or annual earnings of an individual',
+  'apartmentArea': 'the total square footage or square meters of the apartment',
+  'developer': 'the company which develops the building',
+}
+
+const PARAMETERS_EXPLANATION = {
+  floor: 'ste pti bacatrutyun exni amen field i hma vor ai-y aveli xary texteric karena jogi inj data hani u esi pti tanq eli ai-in prompt-i mej'
+}
+
+module.exports = {
+  BASIC_PARAMETERS,
+  PARAMETERS_EXPLANATION,
+  BASIC_PARAMETERS_EXPLANATION
+}
