49884de8690f9d9bedafe8faffe1b269e2311a1f
Nahapet-sb <nahapet.papikyan@softconstruct.com>
2024-06-25 00:37:08 +0400

    changed api

--- RealtyGptFront/src/services/soketConnector.ts
+++ RealtyGptFront/src/services/soketConnector.ts
@@ -1,4 +1,4 @@
 import { io } from 'socket.io-client';
-const url = import.meta.env.MODE === 'development' ? 'http://localhost:3000' : 'https://realtygpt.am/api';
+const url = import.meta.env.MODE === 'development' ? 'http://localhost:3000' : 'https://realtygpt.am/';
 
 export const socket = io(url);
